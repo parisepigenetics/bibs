71a59d49a947fed580d0e5235f796e70131303e7
Magali Hennion <magali.hennion@u-paris.fr>
2023-06-09 11:57:37 +0200

    improve exo

--- documents/templates/05_06_star.sh
+++ documents/templates/05_06_star.sh
@@ -5,7 +5,7 @@
 #SBATCH --job-name=Alignment
 #SBATCH --output=star-alignment-%j.out
 #SBATCH --error=star-alignment-%j.err
-#SBATCH  # increase memory to 25G
+#SBATCH  ?? # increase memory to 25G
 
 ### MODULES ###
 module purge
@@ -13,8 +13,8 @@ module load  ?? # find appropriate STAR module (2.7.5a)
 
 ### VARIABLES ###
 pathToIndex= ?? # look for the path of the index for mus musculus (mm39) made for STAR 
-pathToFastq1= ?? # look in /shared/banks/mus_musculus/test_fastq to get the path to the R1 fastq.gz file
-pathToFastq2= ?? # look in /shared/banks/mus_musculus/test_fastq to get the path to the R2 fastq.gz file
+pathToFastq1= ?? # look in /shared/projects/training/test_fastq to get the path to the R1 fastq.gz file
+pathToFastq2= ?? # look in /shared/projects/training/test_fastq to get the path to the R2 fastq.gz file
 outputFileName= ?? # choose your output file name
 
 ### COMMANDS ###
@@ -23,4 +23,4 @@ STAR --genomeDir $pathToIndex \
 --outFileNamePrefix $outputFileName \
 --readFilesCommand zcat
 
-# --runThreadN ??  # exercice 6 use the Slurm variable to adjust the number of threads to available CPUs
+# --runThreadN ??  # exercice 6 : use 4 threads. Tip! use can a Slurm variable to adjust the number of threads to available CPUs
